58dbd3ffd01c20cc138c89a1aeb57ee5c7e05446
huojian-jan <3300837618@qq.com>
2024-12-29 09:15:05 +0800

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a=100;
+        var b=50;
+
+        var sum=Add(a,b);
+        var diff=Subtract(a,b);
+
+        Console.WriteLine("Sum: "+sum);
+    }
+
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
